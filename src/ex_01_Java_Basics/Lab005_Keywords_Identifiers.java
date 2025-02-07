8c1783a067b10e659b8b9db092fce8580aacaafb
KavyaE55 <ekavya55@gmail.com>
2025-02-07 13:43:35 +0530

    second commit

--- src/ex_01_Java_Basics/Lab005_Keywords_Identifiers.java
+++ src/ex_01_Java_Basics/Lab005_Keywords_Identifiers.java
@@ -3,6 +3,7 @@ package ex_01_Java_Basics;
 public class Lab005_Keywords_Identifiers {
     public static void main(String[] args) {
         System.out.println("The word which arein blue color is a keywords");
+        System.out.println("hii");
     }
   
 }
