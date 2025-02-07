2e26ede32c156f770f450ef1ac79c28d461012bc
KavyaE55 <ekavya55@gmail.com>
2025-02-07 17:54:53 +0530

    9 table

---
+++ src/ex_01_Java_Basics/Lab017_print_9_multiple.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basics;
+
+public class Lab017_print_9_multiple {
+    public static void main(String[] args) {
+        int num = 9;
+
+        System.out.printf("9 x 1 = %d\n", num * 1);
+        System.out.printf("9 x 2 = %d\n", num * 2);
+        System.out.printf("9 x 3 = %d\n", num * 3);
+        System.out.printf("9 x 4 = %d\n", num * 4);
+        System.out.printf("9 x 5 = %d\n", num * 5);
+        System.out.printf("9 x 6 = %d\n", num * 6);
+        System.out.printf("9 x 7 = %d\n", num * 7);
+        System.out.printf("9 x 8 = %d\n", num * 8);
+        System.out.printf("9 x 9 = %d\n", num * 9);
+        System.out.printf("9 x 10 = %d\n", num * 10);
+    }
+}
+//9*1=9
